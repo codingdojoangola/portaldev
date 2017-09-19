e2d051d5afceb1ed62e538e96688fb9162ca53d1
José Cage <josejanuario7@gmail.com>
2017-09-20 00:32:13 +0100

    Apply fixes from StyleCI (#1)
    [ci skip] [skip ci]

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -2,8 +2,8 @@
 
 namespace PortalDev\Http\Controllers\Auth;
 
-use PortalDev\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use PortalDev\Http\Controllers\Controller;
 
 class ForgotPasswordController extends Controller
 {
